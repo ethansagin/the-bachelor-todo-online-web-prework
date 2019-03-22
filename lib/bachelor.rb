cfa1a108a563456d2668e1982e1ab7ea77d61853
Ethan <ethansagin@gmail.com>
2019-03-22 20:01:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,6 @@ def get_occupation(data, hometown)
       girl.each do |k, v|
         if k == "hometown" && v == hometown
           target_hash = girl
-          binding.pry
         end
       end
     end
